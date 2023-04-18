349aca4ed291104e534368ec1a7236d0e920ffbf
Mark Goadrich <goadrich@hendrix.edu>
2023-04-18 18:38:02 -0500

    Happy with robot movement

--- Ubi Sunt/Assets/Scripts/UbiMovement.cs
+++ Ubi Sunt/Assets/Scripts/UbiMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,27 @@ using UnityEngine;
 public class UbiMovement : MonoBehaviour
 {
 
+    public enum State {REST, MOVING, STOPPING};
+
     private Rigidbody2D body;
+    private Animator animator;
+    private Vector3 target;
     public float horizontal;
+    private float lasth;
     public float vertical;
+    private float lastv;
 
-    private float moveLimiter = 0.7f;
+    public State state = State.REST;
+    private bool firstStopping = true;
 
-    public float runSpeed = 5f;
+    public float speed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        target = transform.position;
     }
 
     // Update is called once per frame
@@ -24,14 +34,45 @@ public class UbiMovement : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("horizontal", horizontal);
+        animator.SetFloat("vertical", vertical);
     }
 
     void FixedUpdate() {
         if (horizontal != 0 && vertical != 0) {
-            horizontal *= moveLimiter;
-            vertical *= moveLimiter;
-        }
+            horizontal = 0;
+        } 
 
-        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        if (state == State.REST) {
+            if (horizontal != 0 || vertical != 0) {
+                body.AddForce(new Vector2(horizontal * speed, vertical * speed));
+                state = State.MOVING;
+            }
+        } else if (state == State.MOVING) {
+            if (lasth != horizontal || lastv != vertical) {
+                state = State.STOPPING;
+            } else {
+                body.AddForce(new Vector2(horizontal * speed, vertical * speed));
+            }
+        } else if (state == State.STOPPING && body.velocity.magnitude < 1) {
+            if (firstStopping) {
+            target = new Vector3(Mathf.Floor(transform.position.x) + Math.Max(0, Math.Sign(body.velocity.x)), 
+                Mathf.Floor(transform.position.y) + Math.Max(0, Math.Sign(body.velocity.y)), 
+                Mathf.Floor(transform.position.z));
+                firstStopping = false;
+            }
+
+            Vector2 desired = target - transform.position;
+            if (desired.magnitude < 0.01) {
+                transform.position = target;
+                body.velocity = Vector2.zero;
+                state = State.REST;
+                firstStopping = true;
+            } else {
+                body.AddForce(desired.normalized * speed - body.velocity);
+            }
+        }
+        lasth = horizontal;
+        lastv = vertical;
     }
 }
